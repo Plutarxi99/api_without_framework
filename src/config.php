1e353ca81d749a89084e47b65ca3c2f166c3a0c0
Egor Shievanov <shievanov@bk.ru>
2025-10-12 13:54:11 +0300

    add config for db connect

---
+++ src/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dsn' => 'mysql:host=127.0.0.1;dbname=mailing;charset=utf8mb4',
+    'user' => 'root',
+    'pass' => '',
+    'uploadDir' => __DIR__ . '/../storage/uploads',
+    'batchSize' => 500,
+];
